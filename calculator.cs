d8850d6587b7e8668e61184b627015213361435c
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2024-08-02 19:27:15 +0300

    update
    update

--- calculator.cs
+++ calculator.cs
@@ -2,19 +2,55 @@ namespace calculator
 {
     internal class Program
     {
-        public double num1,num2;
+        
 
         static void Main(string[] args)
         {
+            double num1, num2;
+            string operation;
+
             Console.WriteLine("CALCULATOR");
             Console.WriteLine("------------");
-            
-        }
-        static void operation()
-        {
+
+            Console.Write("First number: ");
+            num1=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Second Number: ");
+            num2=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Choose your operations: (+ - * /)  : ");
+            operation=Console.ReadLine();
+
+            double result = 0;
+
+            switch (operation) 
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 !=0)
+                    {
+                        result = num1 / num2;
 
 
+                    }
+                    else
+                    {
+                        Console.WriteLine("yo, you made a mistake!");
+                    }
+                   break;
+
+            }
+            Console.Write("RESULT: " + result);
         }
-        dotnet run
+        
     }
 }
