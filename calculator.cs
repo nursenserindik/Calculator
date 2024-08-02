5185f9169026e7e33d5ccea2fff4cd150ee8aa1d
nursenserindik <121162673+nursenserindik@users.noreply.github.com>
2024-08-02 18:50:14 +0300

    calculator

---
+++ calculator.cs
@@ -0,0 +1,20 @@
+namespace calculator
+{
+    internal class Program
+    {
+        public double num1,num2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATOR");
+            Console.WriteLine("------------");
+            
+        }
+        static void operation()
+        {
+
+
+        }
+        dotnet run
+    }
+}
